21a590fe88b9fa29b78e8183f2f04e5b2abfec75
Sam <samsmith453@gmail.com>
2017-03-04 23:38:16 +0000

    finished version

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 var express = require("express");
+var strftime = require("strftime");
 
-var app = express()
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get("/:date", function(req, res){
+    var date = req.params.date;
+    var num = Number(date);
+    var datify = new Date(date);
+    if(!isNaN(num)){
+        var obj = {
+            "unix": num,
+            "human": strftime('%o %B %Y', new Date(num*1000))
+        };
+        res.send(obj);
+    }
+    else if(datify!="Invalid Date"){
+        var obj = {
+            "unix": datify.getTime()/1000,
+            "human": strftime('%o %B %Y', datify)
+        };
+        res.send(obj);
+    }
+    
+    else{
+        var now = new Date();
+        var obj = {
+            "unix": now.getTime(),
+            "human": strftime('%o %B %Y', now)
+        }
+        res.send(obj)
+    }
+    
+    res.end();
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.use(express.static("public"));
+
+app.listen(8080, function(){
+    console.log("Connected");
+});
